7d0086f2160a5368e1fd6a4438ae0b1256f88628
Suldayan <160978068+Suldayan@users.noreply.github.com>
2026-08-07 13:10:44 -0700

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.java
@@ -0,0 +1,37 @@
+class Solution {
+    private boolean isClosingBracket(final Character bracket) {
+        return switch (bracket) {
+            case ')', '}', ']' -> true;
+            default -> false;
+        };
+    }
+
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            final char c = s.charAt(i);
+            
+            if (!isClosingBracket(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                final char openningBracket = stack.pop();
+                if (openningBracket == '(' && c != ')') {
+                    return false;
+                    }
+                if (openningBracket == '[' && c != ']') {
+                        return false;
+                    } 
+                if (openningBracket == '{' && c != '}') {
+                        return false;
+                    }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
