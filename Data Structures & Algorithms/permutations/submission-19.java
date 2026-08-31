8a43459bef53527bab66710ba943586d0071d8de
Suldayan <160978068+Suldayan@users.noreply.github.com>
2026-08-30 19:42:41 -0700

    Add: permutations - submission-19

---
+++ Data Structures & Algorithms/permutations/submission-19.java
@@ -0,0 +1,36 @@
+class Solution {
+    private void findPerm(
+        List<List<Integer>> result, 
+        final int[] nums,
+        List<Integer> subset,
+
+        boolean[] isVisited) 
+    {
+        if (subset.size() == nums.length) {
+            result.add(new ArrayList<>(subset));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (isVisited[i]) {
+                continue;
+            }
+
+            isVisited[i] = true;
+            subset.add(nums[i]);
+            findPerm(result, nums, subset, isVisited);
+            subset.remove(subset.size() - 1);
+            isVisited[i] = false;
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] isVisited = new boolean[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            isVisited[i] = false;
+        }
+
+        findPerm(result, nums, new ArrayList<>(), isVisited);
+        return result;
+    }
+}
